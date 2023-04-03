968fdf75f16af5b6087323e5590e2b6c81f55894
BensonTerra <benson.terra@hotmail.com>
2023-04-03 19:45:24 +0100

    save--03/04/23--19:45

---
+++ ES06/EX01i/script.js
@@ -0,0 +1,121 @@
+const games = 
+[
+    { name: 'Rick Dangerous', year: 1989 },//obejeto 1
+    { name: 'Goblins', year: 1992 },//obejeto 2
+    { name: 'Golden Axe', year: 1989 }//obejeto 3
+] // Lista com objetos
+
+function showGames(games)
+{
+    for (let game of games)
+    {
+        console.log(`Name: ${game.name} / Year: ${game.year}`)
+    }
+}
+showGames(games)
+
+function showNumberGames(games)
+{
+    console.log(`Numeros de jogos: ${games.length}`)
+}
+showNumberGames(games)
+
+const frmAdd = document.getElementById('addForm')
+function addGame(event)
+{
+    event.preventDefault()
+    const txtNameAdd = document.getElementById('addName')
+    console.log(txtNameAdd.value)
+    if (isExistGame(games, txtNameAdd) === true)
+    {
+        return alert('Jogo já cadastrado')
+    }
+    else
+    {
+        const txtYear = document.getElementById('addYear')
+        console.log(txtYear.value)
+        
+        newGame =
+        {
+            name: txtNameAdd.value, year: parseInt(txtYear.value)
+        }
+        games.push(newGame)
+        console.table(games)
+        averageLaunchDate(games)
+        frmAdd.reset()
+        return true
+    }
+    
+}
+frmAdd.addEventListener('submit', function(event){addGame(event)})
+
+function isExistGame(games, txtName)
+{
+    if (games.some(game => game.name === txtName.value))
+    {
+        return true
+    }
+    else
+    {
+        return false
+    }
+}
+
+function averageLaunchDate(games)
+{
+    let average = 0
+    for (let game of games)
+    {
+        average += game.year
+    }
+    average = average / games.length
+    console.log(average)
+}
+averageLaunchDate(games)
+
+const frmRemove = document.getElementById('removeForm')
+function removeGame(event)
+{
+    event.preventDefault()
+    const txtNameRemove = document.getElementById('removeName')
+    console.log(txtNameRemove.value)
+    if (isExistGame(games, txtNameRemove) === true)
+    {
+        const indexGame = games.findIndex(game => game.name === txtNameRemove.value)
+        console.log(indexGame)
+        games.splice(indexGame, 1)
+        console.table(games)
+        averageLaunchDate(games)
+        frmRemove.reset()
+    }
+}
+frmRemove.addEventListener('submit', function(event){removeGame(event)})
+
+function gamesFrom90(games)
+{   
+    let gamesAll90 = []
+    for (let game of games)
+    {
+        if (game.year >= 1990 && game.year <= 2000)
+        {
+            let game90 = 
+            {
+                name: game.name, year: game.year
+            }
+            gamesAll90.push(game90)
+            console.table(gamesAll90)
+        }
+    }
+}
+gamesFrom90(games)
+
+function concatenateGamesNames(games)
+{
+    let gamesNames = ""
+    for (let game of games)
+    {
+        gamesNames += game.name + ","
+    }
+    console.log(gamesNames)
+}
+concatenateGamesNames(games)
---
+++ ES06/EX02/script.js
@@ -0,0 +1,138 @@
+const movies = 
+[
+    {
+        title: 'The Shawshank Redemption',
+        genre: 'Drama',
+        year: 1994,
+        cover: 'https://m.media-amazon.com/images/I/519NBNHX5BL.jpg',
+        trailer: 'https://www.youtube.com/embed/6hB3S9bIaco',
+    },
+    {
+        title: 'The Godfather',
+        genre: 'Drama',
+        year: 1972,
+        cover: 'https://m.media-amazon.com/images/I/41+HJCUl3EL.jpg',
+        trailer: 'https://www.youtube.com/embed/5DO-nDW43Ik',
+    },
+    {
+        title: 'The Godfather1',
+        genre: 'Drama',
+        year: 1972,
+        cover: 'https://m.media-amazon.com/images/I/41+HJCUl3EL.jpg',
+        trailer: 'https://www.youtube.com/embed/5DO-nDW43Ik',
+    },
+    {
+        title: 'The Godfather2',
+        genre: 'Drama',
+        year: 1972,
+        cover: 'https://m.media-amazon.com/images/I/41+HJCUl3EL.jpg',
+        trailer: 'https://www.youtube.com/embed/5DO-nDW43Ik',
+    },
+];
+
+console.table(movies);
+
+//--//--**
+const txtMovieName = document.getElementById('addTitle');
+//console.log(txtMovieName.value);
+//---//
+const txtMovieGenre = document.getElementById('addGenre');
+//console.log(txtMovieGenre.value);
+//---//
+const txtMovieYear = document.getElementById('addYear');
+//console.log(txtMovieYear.value);
+const dateNow = new Date();
+txtMovieYear.max = dateNow.getFullYear();
+//---//
+const txtMovieCover = document.getElementById('addCover');
+//console.log(txtMovieCover.value);
+//---//
+const txtMovieTrailer = document.getElementById('addTrailer');
+//console.log(txtMovieTrailer.value);
+//---//--**
+const addForm = document.getElementById('addForm');
+//console.log(addForm);
+//---//
+const tblBody = document.getElementById('tableBody');
+//console.log(tblBody);
+//---//---//
+const dialogCover = document.getElementById('dialogCover');
+//console.log(dialogCover);
+//--//
+const dialogTrailer = document.getElementById('dialogTrailer');
+//console.log(dialogTrailer);
+//--//--**
+
+function renderMovies()
+{
+    let template = '';
+    for (let movie of movies)
+    {
+        template += 
+        `
+        <tr>
+            <td>${movie.title}</td>
+            <td>${movie.genre}</td>
+            <td>
+                <button onClick = "openCover('${movie.cover}')">SEE COVER</button>
+                <button onClick = "openTrailer('${movie.trailer}')">SEE TRAILER</button>
+                <button onClick = "removeMovie('${movie.title}')">REMOVE</button>
+            </td>
+        </tr>
+        `
+    }
+    tblBody.innerHTML = template;
+}
+renderMovies(movies);
+
+const frmMovieAdd = document.getElementById('addForm');
+function addMovie(event)
+{
+    event.preventDefault();
+    console.log("Function addMovie");
+    const newMovie =
+    {
+        title: txtMovieName.value,
+        genre: txtMovieGenre.value,
+        year: txtMovieYear.value,
+        cover: txtMovieCover.value,
+        trailer: txtMovieTrailer.value,
+    }
+    if (movies.some(movie => movie.title === newMovie.title))
+    {
+        return alert("Movie already exists");
+    }
+    
+    movies.push(newMovie);
+    renderMovies();
+    console.table(movies);
+    addForm.reset();
+    return true;
+}
+frmMovieAdd.addEventListener('submit', function(event){addMovie(event)});
+
+function removeMovie(title)
+{
+    const index = movies.findIndex(movie => movie.title === title);
+    console.log(index);
+    movies.splice(index, 1);
+    console.table(movies);
+    renderMovies();
+}
+
+function openCover(source)
+{
+    const img = document.querySelector('img');
+    img.src = source;
+    console.log(img);
+    dialogCover.showModal();/*      .showModal()        */
+}
+
+function openTrailer(source)
+{
+    const iFrame = document.querySelector('iframe');
+    iFrame.src = source;
+    console.log(iFrame);
+    dialogTrailer.showModal();/*      .showModal()        */
+}
+
