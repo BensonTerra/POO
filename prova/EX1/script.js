7f38711fcce21cd913cb89223eae5ee332221df8
BensonTerra <benson.terra@hotmail.com>
2023-05-02 15:53:40 +0100

    pessoal

--- prova/EX1/script.js
+++ prova/EX1/script.js
@@ -1,10 +1,16 @@
 const array1 = [1,3,2]
+console.table(array1)
 const array2 = [5,4,3]
-const novoArray = concatenaArrays(array1, array2)
+console.table(array2)
+const novoArray = concatenaArrays(array1,array2)
+
+
 
 function concatenaArrays(array1, array2)//Função que permite concatinar os array
 {
     array1.sort()
+    array2.sort()
+    console.log(array1)
     array1.forEach(element => 
         {
             if (array2.includes(element))//eliminar elementos repetidos
@@ -18,7 +24,6 @@ function concatenaArrays(array1, array2)//Função que permite concatinar os arr
         }
     );
     //console.table(array1)
-    array2.sort()
     //console.table(array2)
     let newArray = array1.concat(array2)
     console.table(newArray)
