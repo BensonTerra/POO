9c9b0729be386432c5a8b1fd87285fef527e5f92
BensonTerra <benson.terra@hotmail.com>
2023-05-17 12:25:33 +0100

    bwasf

---
+++ organizar/EX-8/teste.js
@@ -0,0 +1,10 @@
+const btnLogin = document.getElementById("btnLogin");
+btnLogin.addEventListener('click', teste)
+
+const btnRegistro = document.getElementById("btnRegistro");
+btnRegistro.addEventListener('click', teste)
+
+function teste()
+{
+    console.log("teste")
+}
