27d5f365847dc1383911500bad8b3049df20de17
BensonTerra <benson.terra@hotmail.com>
2023-05-18 15:46:30 +0100

    qwedf

--- organizar/EX08/js/views/BandCatalogView.js
+++ organizar/EX08/js/views/BandCatalogView.js
@@ -23,18 +23,20 @@ function generateCard(band)
 
     result =
     `
-    <div class="card" style="width:18rem;">
-        <img src="${band.linkCapa}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${band.name}</h5>
-            <h6 class="card-subtitle mb-2 text-muted ">${band.gender}</h6>
-            <button id = "${band.name}" class="btn btn-primary view">Ver mais</button>
+    <div  class="col">
+        <div class="card mb-3" style="widht: 300px;">
+            <img src="${band.linkCapa}" class="card-img-top" style="height: 300px;">
+            <div class="card-body">
+                <h5 class="card-title">${band.name}</h5>
+                <h6 class="card-text">${band.gender}</h6>
+                <button id = "${band.name}" class="btn btn-primary view">Ver mais</button>
 
     `
 
 
     result += 
     `
+            </div>
         </div>
     </div>
     `
